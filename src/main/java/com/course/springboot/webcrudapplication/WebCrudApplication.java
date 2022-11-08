428ea0f1f7d85003a5c28df0e0d52ebc27904340
Taras Buriak <tarasburyak@gmail.com>
2022-11-08 21:01:08 +0200

    Initial commit

---
+++ src/main/java/com/course/springboot/webcrudapplication/WebCrudApplication.java
@@ -0,0 +1,13 @@
+package com.course.springboot.webcrudapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCrudApplication.class, args);
+	}
+
+}
